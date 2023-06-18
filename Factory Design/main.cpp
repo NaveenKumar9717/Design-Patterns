fd76b56272e5611b6521e106be429c6d2c4f6f4d
naveen <naveencruz20@gmail.com>
2023-06-18 20:32:30 +0530

    Initial Commit

---
+++ Factory Design/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Factory Design
+//
+//  Created by Naveen Kumar on 18/06/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
